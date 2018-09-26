4037784e3a12719e2ce3fdc90404176307464c75
unknown <gui85238523@gmail.com>
2018-09-26 10:17:09 -0400

    projeto av1

---
+++ App.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Button, View, Text } from 'react-native';
+import { createStackNavigator } from 'react-navigation'; // Version can be specified in package.json
+
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Olá, bem-vindo a AV1. Clique no botão abaixo</Text>
+        <Button
+          title="Ir para a proxima tela!"
+          onPress={() => this.props.navigation.navigate('Details')}
+        />
+      </View>
+    );
+  }
+}
+
+class DetailsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>
+          Nome: Guilherme Sampaio
+              Matricula: 0050014286
+              link para o github com este código:
+              https://github.com/GuiSampaio/react-native-app.git
+              </Text>
+        <Button
+          title="Voltar"
+          onPress={() => this.props.navigation.navigate('Home')}
+        />
+      </View>
+    );
+  }
+}
+
+const RootStack = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Details: DetailsScreen,
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
+
+export default class App extends React.Component {
+  render() {
+    return <RootStack />;
+  }
+}
